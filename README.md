c159ca286e0d7480154540c050cbb71407d99453
tsaniawarda2 <63621268+tsaniawarda2@users.noreply.github.com>
2026-07-19 10:57:28 +0700

    setup

---
+++ README.md
@@ -0,0 +1,2 @@
+# dummyjson-api-playwright
+
---
+++ config/env.ts
@@ -0,0 +1,14 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+function getEnv(key: string): string {
+  const value = process.env[key];
+  if (!value) {
+    throw new Error(`Missing required environment variable: ${key}`);
+  }
+  return value;
+}
+
+export const ENV = {
+  BASE_URL: getEnv("BASE_URL"),
+};
