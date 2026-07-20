a7c84c3a80d283ffac3a9e85a7f45659c229bcb3
tsaniawarda2 <63621268+tsaniawarda2@users.noreply.github.com>
2026-07-20 17:44:46 +0700

    refactor: implement AuthApi for authentication tests

--- tests/authentication/login.spec.ts
+++ tests/authentication/login.spec.ts
@@ -1,29 +1,10 @@
 import { test, expect } from "@playwright/test";
 import { loginData } from "../../test-data/authentication";
 import { AuthApi } from "../../api/AuthApi";
-// test("TC_LOGIN_001 - Login with valid credentials", async ({ request }) => {
-//   const response = await request.post("/auth/login", {
-//     // data:loginData.{
-//     //   username: "emilys",
-//     //   password: "emilyspass",
-//     // },
-//     data: loginData.valid,
-//   });
-
-//   const body = await response.json();
-//   // console.log(JSON.stringify(response, null, 2));
-//   // console.log(body);
-
-//   expect(response.status()).toBe(200);
-//   expect(body.id).toBeGreaterThan(0);
-//   expect(body.username).toBe(loginData.valid.username);
-//   expect(body.accessToken).toBeTruthy();
-//   expect(body.refreshToken).toBeTruthy();
-// });
 test("TC_LOGIN_001 - Login with valid credentials", async ({ request }) => {
- const authApi = new AuthApi(request)
+  const authApi = new AuthApi(request);
 
-  const response = await authApi.login(loginData.valid)
+  const response = await authApi.login(loginData.valid);
 
   const body = await response.json();
 
@@ -33,9 +14,9 @@ test("TC_LOGIN_001 - Login with valid credentials", async ({ request }) => {
 });
 
 test("TC_LOGIN_002 - Login with invalid username", async ({ request }) => {
-  const response = await request.post("/auth/login", {
-    data: loginData.invalidUsername,
-  });
+  const authApi = new AuthApi(request);
+
+  const response = await authApi.login(loginData.invalidUsername);
 
   const body = await response.json();
 
@@ -45,9 +26,9 @@ test("TC_LOGIN_002 - Login with invalid username", async ({ request }) => {
 });
 
 test("TC_LOGIN_003 - Login with invalid password", async ({ request }) => {
-  const response = await request.post("/auth/login", {
-    data: loginData.invalidPassword,
-  });
+  const authApi = new AuthApi(request);
+
+  const response = await authApi.login(loginData.invalidPassword);
 
   const body = await response.json();
 
@@ -56,22 +37,20 @@ test("TC_LOGIN_003 - Login with invalid password", async ({ request }) => {
 });
 
 test("TC_LOGIN_004 - Login without username", async ({ request }) => {
-  const response = await request.post("/auth/login", {
-    data: loginData.withoutUsername,
-  });
+  const authApi = new AuthApi(request);
+
+  const response = await authApi.login(loginData.withoutUsername);
 
   const body = await response.json();
-  // console.log(response.status());
-  // console.log(body);
 
   expect(response.status()).toBe(400);
   expect(body.message).toBe("Username and password required");
 });
 
 test("TC_LOGIN_005 - Login without password", async ({ request }) => {
-  const response = await request.post("/auth/login", {
-    data: loginData.withoutPassword,
-  });
+  const authApi = new AuthApi(request);
+
+  const response = await authApi.login(loginData.withoutPassword);
 
   const body = await response.json();
 
@@ -80,9 +59,9 @@ test("TC_LOGIN_005 - Login without password", async ({ request }) => {
 });
 
 test("TC_LOGIN_006 - Login with empty request body", async ({ request }) => {
-  const response = await request.post("/auth/login", {
-    data: loginData.emptyBody,
-  });
+  const authApi = new AuthApi(request);
+
+  const response = await authApi.login(loginData.emptyBody);
 
   const body = await response.json();
 
